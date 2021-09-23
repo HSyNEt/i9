17f8db782e9f513592a0d7794fe94f6e6af5960f
Ben Jaber <sunghsm@gmail.com>
2021-09-23 10:31:11 +0100

    Add project files.

---
+++ i9/Models/iCarts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace i9.Models
+{
+    public class iCarts
+    {
+        public int Id { get; set; }
+        public string account_id { get; set; }
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+
+        public string product_descript { get; set; }
+        public string product_price { get; set; }
+        public int product_qua { get; set; }
+        public string product_img { get; set; }
+    }
+}
---
+++ i9/Models/iOrders.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace i9.Models
+{
+    public class iOrders
+    {
+        public int id { get; set; }
+        public int product_id { get; set; }
+
+        public string product_name { get; set; }
+
+        public string account_id { get; set; }
+
+        public string order_delivery { get; set; }
+
+        public string order_price { get; set; }
+    }
+}
